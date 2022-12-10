a7537943f3bd4b752ba9fbb9e709d920101ba61a
harsh6975 <harshu1234ashv@gmail.com>
2022-12-10 12:08:33 +0530

    User Input

--- Input.java
+++ Input.java
@@ -3,23 +3,23 @@ import java.util.*;
 
 public class Input {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            // input a string
+            String name = sc.next();
+            System.out.println(name);
 
-        // input a string
-        String name = sc.next();
-        System.out.println(name);
+            // input a line
+            String line = sc.nextLine();
+            System.out.println(line);
 
-        // input a line
-        String line = sc.nextLine();
-        System.out.println(line);
+            // input a integer
+            int a = sc.nextInt();
+            System.out.println(a);
 
-        // input a integer
-        int a = sc.nextInt();
-        System.out.println(a);
-
-        // input a float
-        float b = sc.nextFloat();
-        System.out.println(b);
+            // input a float
+            float b = sc.nextFloat();
+            System.out.println(b);
+        }
 
     }
 }
