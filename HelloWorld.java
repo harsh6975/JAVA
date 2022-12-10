67badcbf0071041951021b6529035b5da20ac7c3
harsh6975 <harshu1234ashv@gmail.com>
2022-12-10 12:05:17 +0530

    User Input

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String args[]) {
+        // print with new line
+        System.out.println("Hello World with new line using println");
+
+        // print with new line
+        System.out.print("Hello World with new line using backslac n and print\n");
+
+        // print without new line
+        System.out.print("Hello World without new line using print");
+
+    }
+}
---
+++ Input.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // input a string
+        String name = sc.next();
+        System.out.println(name);
+
+        // input a line
+        String line = sc.nextLine();
+        System.out.println(line);
+
+        // input a integer
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        // input a float
+        float b = sc.nextFloat();
+        System.out.println(b);
+
+    }
+}
